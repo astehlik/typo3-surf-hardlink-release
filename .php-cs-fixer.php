ce47028d5c8d3692017e4cecc713e63fccf00c88
Alexander Stehlik <alex@stehlik-online.de>
2024-11-07 10:15:11 +0100

    [TASK] Update dependencies for TYPO3 13

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -8,7 +8,10 @@ $finder = PhpCsFixer\Finder::create()
         ]
     );
 
+$rules = include __DIR__ . '/vendor/de-swebhosting/php-codestyle/PhpCsFixer/PerCsDefaultRules.php';
+$rules['no_superfluous_phpdoc_tags'] = ['allow_mixed' => true, 'remove_inheritdoc' => false];
+
 $config = new PhpCsFixer\Config();
-return $config->setRules(include __DIR__ . '/vendor/de-swebhosting/php-codestyle/PhpCsFixer/Psr12DefaultRules.php')
+return $config->setRules($rules)
     ->setRiskyAllowed(true)
     ->setFinder($finder);
